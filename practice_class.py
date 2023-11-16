ee1454820c2221cb539bfb2a10681cfe557cf153
Chivu23 <chivu1312g@gmail.com>
2023-11-16 13:37:52 +0200

    def_class

---
+++ practice_class.py
@@ -0,0 +1,31 @@
+"""
+define a class name "Calculator", that:
+- have 2 attribute on object ( init): a. b
+-method:
+    - sum
+    - reduction
+    - multiplication
+    - division
+"""
+
+
+class Calculator:
+
+    # initialization method /  construct
+    def __init__(self, a, b):
+        self.a = a          # def attrib a take val a from constructor
+        self.b = b          # def attrib b take val from b value from constructor
+
+    def sum(self):
+        pass
+
+    def reduction(self):
+        pass
+
+    def multiplication(self):
+        pass
+
+    def division(self):
+        pass
+
+
