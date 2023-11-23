cb3c2a6e703d0b3d4f787a20ec7f4f01c3fd31c4
Chivu23 <chivu1312g@gmail.com>
2023-11-23 13:54:26 +0200

    exe_constructor

--- four_basic_principles_of_OOP.py
+++ four_basic_principles_of_OOP.py
@@ -8,72 +8,75 @@
 4. ENCAPSULATION
 
 """
+
+
 #  Models, Photos, Specs & Engines (1968-Present)
+#
+# class Opel:
+#
+#     def __init__(self, year, fuel):
+#         self.year = year
+#         self.fuel = fuel
+#
+#     def speed_check(self):
+#         print("220 km/h")
+#
+#     def break_check(self):
+#         print("5 m")
+#
+#     def describe(self):
+#         print(f"The car is made in {self.year} and is use {self.fuel} power")
+#
+#
+# class CorsaOpel(Opel):
+#     """
+#     To add new properties:
+#      1. We expand the list of parameters that the __init__ method can take them.
+#      2. We call the __init__ from the parent class, using super(), with the necessary parameters for the parent class.
+#     """
+#
+#     def __init__(self, year, fuel, sound, color):
+#         super().__init__(year, fuel)
+#         self.sound = sound
+#         self.color = color
+#
+#     def describe(self):
+#         print(f"The color of that car is {self.color}")
+#         print(f"That car make {self.sound} sound")
+#
+#
+# # child class ---> inheritance from  class Opel
+#
+#
+# class AstraOpelH(Opel):
+#
+#     def motor_check(self):
+#         print("1923 r/m")
+#
+#
+# car4 = Opel(year=2023, fuel="diesel")
+# # print(car4.fuel)
+# # print(car4.year)
+# # car4.describe()
+#
+# car5 = CorsaOpel(year=2000,
+#                  fuel="benzine",
+#                  sound="nino-mimo",
+#                  color="red")
+#
+# print(car5.year)
+# print(car5.fuel)
+# print(car5.sound)
+# print(car5.color)
+#
+# car5.describe()
+#
+#
+# class AstraOpelG(Opel):
+#
+#     def pollution_check(self):
+#         print("23 µg/m3")
 
-class Opel:
-
-    def __init__(self, year, fuel):
-        self.year = year
-        self.fuel = fuel
-
-    def speed_check(self):
-        print("220 km/h")
-
-    def break_check(self):
-        print("5 m")
-
-    def describe(self):
-        print(f"The car is made in {self.year} and is use {self.fuel} power")
-
-
-class CorsaOpel(Opel):
-
-    """
-    To add new properties:
-     1. We expand the list of parameters that the __init__ method can take them.
-     2. We call the __init__ from the parent class, using super(), with the necessary parameters for the parent class.
-    """
-
-    def __init__(self, year, fuel, sound, color):
-        super().__init__(year, fuel)
-        self.sound = sound
-        self.color = color
-
-    def describe(self):
-        print(f"The color of that car is {self.color}")
-        print(f"That car make {self.sound} sound")
-
-# child class ---> inheritance from  class Opel
-
-
-class AstraOpelH(Opel):
-
-    def motor_check(self):
-        print("1923 r/m")
-
-
-car4 = Opel(year=2023, fuel="diesel")
-# print(car4.fuel)
-# print(car4.year)
-# car4.describe()
-
-car5 = CorsaOpel(year=2000,
-                 fuel="benzine",
-                 sound="nino-mimo",
-                 color="red")
-
-print(car5.year)
-print(car5.fuel)
-print(car5.sound)
-print(car5.color)
-
-car5.describe()
-
-
-class AstraOpelG(Opel):
-
-    def pollution_check(self):
-        print("23 µg/m3")
 
 #
 # car1 = Opel(2022)
@@ -98,3 +101,21 @@ class AstraOpelG(Opel):
 # print(car3.year)
 
 
+class Snow:
+    # fields (attributes)
+    length = 156
+    brand = "Burton"
+
+    def __init__(self, model, color):     # <---- CONSTRUCTOR
+        self.model = model
+        self.color = color
+
+
+board1 = Snow('Process', 'black')
+board2 = Snow('Ripcord', 'red')
+
+print(board1.model)
+print(board1.color)
+
+print(board2.model)
+print(board2.color)
